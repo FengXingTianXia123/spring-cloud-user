5c2a24f71129b42e34ee654a86095ec9c3f8c3d3
gaishouwen <1042168035@qq.com>
2018-06-12 16:38:50 +0800

    auth项目修改yml

--- user-auth-server/src/main/java/com/user/auth/server/UserAuthServerApplication.java
+++ user-auth-server/src/main/java/com/user/auth/server/UserAuthServerApplication.java
@@ -1,9 +1,11 @@
 package com.user.auth.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.user"})
+@MapperScan("com.user.info.mapper")
 public class UserAuthServerApplication {
 
 	public static void main(String[] args) {
