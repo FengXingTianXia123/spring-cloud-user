84fb765feab888f19be437b30f1c5d62fbcd07ea
gaishouwen <1042168035@qq.com>
2018-06-15 09:49:11 +0800

    登录实体

---
+++ user-auth-server/src/main/java/com/user/auth/entity/UserLogin.java
@@ -0,0 +1,43 @@
+package com.user.auth.entity;
+
+public class UserLogin {
+    private Long id;
+
+    private String userName;
+
+    private Integer type;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
---
+++ user-auth-server/src/main/java/com/user/auth/mapper/UserLoginMapper.java
@@ -0,0 +1,17 @@
+package com.user.auth.mapper;
+
+import com.user.auth.entity.UserLogin;
+
+public interface UserLoginMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserLogin record);
+
+    int insertSelective(UserLogin record);
+
+    UserLogin selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserLogin record);
+
+    int updateByPrimaryKey(UserLogin record);
+}
